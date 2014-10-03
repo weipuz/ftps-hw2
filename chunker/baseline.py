3226452530adf88200e8bd07c13225386f6049f1
fzhan <fzhan@fzhan-X202E.(none)>
2014-10-03 14:59:44 -0700

    i know it

--- chunker/baseline.py
+++ chunker/baseline.py
@@ -32,11 +32,12 @@ import perc
 import sys, optparse, os, codecs
 from collections import defaultdict
 
-def perc_train(train_data, tagset, n):
+
+def perc_train(train_data, tagset, numepochs):
     feat_vec = defaultdict(int)
     # insert your code here
     ct = ChunkTrainer()
-    feat_vec = ct(train_data, tagset, n)
+    feat_vec = ct(train_data, tagset, numepochs)
     # please limit the number of iterations of training to n iterations
     return feat_vec
 
@@ -56,8 +57,15 @@ class ChunkTrainer():
 		ans = ""
 		if isinstance(item, (int, long, float, complex)):
 			ans = repr(item)
+		elif isinstance(item, (type)):
+			ans = item.__name__
+		elif isinstance(item, (defaultdict)):
+			ans = type(item).__name__ + "("
+			for index, value in item.iteritems():
+				ans += self.stringify(index) + ": " + self.stringify(value) + ", "
+			ans += ")"
 		elif isinstance(item, (list, tuple, dict)):
-			ans = "("
+			ans = type(item).__name__ + "("
 			for index, value in enumerate(item):
 				ans += self.stringify(index) + ": " + self.stringify(value) + ", "
 			ans += ")"
@@ -70,7 +78,36 @@ class ChunkTrainer():
 		self.test_file.write(self.stringify(output) + "\n")
 
 	def __call__(self, train_data, tagset, iter_num):
-		return self.chunk(train_data[0][0], train_data[0][1], tagset, iter_num)
+		feat_vec = defaultdict(int)
+		for i in range(0, len(train_data)):
+			labeled_list = train_data[i][0]
+			feat_list = train_data[i][1]
+			#for feat in feat_list:
+				#if not(feat in feat_vec):
+					#feat_vec[feat] = 0
+			for index, line in enumerate(labeled_list):
+				zero = labeled_list[index].split()
+				feat_vec[("U02:" + zero[0], zero[2])] += 1
+				feat_vec[("U12:" + zero[1] + "q", zero[2])] += 1
+				if index > 2:
+					neg2 = labeled_list[index - 2].split()
+					feat_vec[("U00:" + neg2[0], zero[2])] += 1
+					feat_vec[("U10:" + neg2[1], zero[2])] += 1
+				if index > 1:
+					neg1 = labeled_list[index - 1].split()
+					feat_vec[("U01:" + neg1[0], zero[2])] += 1
+					feat_vec[("U05:" + neg1[0] + "/" + zero[0], zero[2])] += 1
+					feat_vec[("U11:" + neg1[1], zero[2])] += 1
+				if index + 1 < len(labeled_list):
+					pos1 = labeled_list[index + 1].split()
+					feat_vec[("U03:" + pos1[0], zero[2])] += 1
+					feat_vec[("U06:" + zero[0] + "/" + pos1[0], zero[2])] += 1
+					feat_vec[("U13:" + pos1[1], zero[2])] += 1
+				if index + 2 < len(labeled_list):
+					pos2 = labeled_list[index + 2].split()
+					feat_vec[("U04:" + pos2[0], zero[2])] += 1
+					feat_vec[("U14:" + pos2[1], zero[2])] += 1
+		return feat_vec
 
 	def chunk(self, labeled_list, feat_list, tagset, iter_num):
 		self.printTest("iter_num: ")
@@ -89,7 +126,7 @@ class ChunkTrainer():
 		# T: number of iterations over the training set. opts.n in default.py
 		T = iter_num
 		# phi: function that maps history/output-label pairs to d-dimensional feature vectors. phi for each history is provided in data/train.feats.gz
-		# phi: Each component of the feature vector is called a feature function: phi s(h,t) where there are d feature functions, s=1,…,d.
+		# phi: Each component of the feature vector is called a feature function: phi s(h,t) where there are d feature functions, s=1,...,d.
 		phi = feat_list
 		d = len(phi)
 		# w: Each sentence is a sequence of words and other useful information such as part-of-speech tags which we refer to as w[1:n].
@@ -97,14 +134,14 @@ class ChunkTrainer():
 		w = [0] * n
 		# t: Each sequence of words is associated with a sequence of output labels t[1:n].
 		t = [None] * n
-		# h: At each point there is a history which is the context in which the output label is assigned to a particular word wi. A history is a three-tuple: h=(t−1,w[1:n],i), where t−1 is the output label for wi−1.
+		# h: At each point there is a history which is the context in which the output label is assigned to a particular word wi. A history is a three-tuple
 		h = ()
 		# Phi: global feature vector defined as above by summing over all local feature vectors phi
 		Phi = [0] * len(labeled_list)
 		for i in range(0, n - 1):
 			Phi[i] = 0
 			for j in range(0, n - 1):
-				Phi[i] += phi[]
+				Phi[i] += phi[j]
 		# w: d dimensional weight vector. one weight for each feature in the feature vector.
 
 		## Initialization ##
@@ -123,12 +160,12 @@ class ChunkTrainer():
 		self.printTest("\n".join(conll_format(w, labeled_list)))
 		return w
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     optparser = optparse.OptionParser()
     optparser.add_option("-t", "--tagsetfile", dest="tagsetfile", default=os.path.join("data", "tagset.txt"), help="tagset that contains all the labels produced in the output, i.e. the y in \phi(x,y)")
     optparser.add_option("-i", "--trainfile", dest="trainfile", default=os.path.join("data", "train.txt.gz"), help="input data, i.e. the x in \phi(x,y)")
     optparser.add_option("-f", "--featfile", dest="featfile", default=os.path.join("data", "train.feats.gz"), help="precomputed features for the input data, i.e. the values of \phi(x,_) without y")
-    optparser.add_option("-n", "--numiterations", dest="n", default=int(10), help="number of iterations of training")
+    optparser.add_option("-e", "--numepochs", dest="numepochs", default=int(10), help="number of epochs of training; in each epoch we iterate over over all the training examples")
     optparser.add_option("-m", "--modelfile", dest="modelfile", default=os.path.join("data", "default.model"), help="weights for all features stored on disk")
     (opts, _) = optparser.parse_args()
 
@@ -142,6 +179,5 @@ if __name__ == "__main__":
     print >>sys.stderr, "reading data ..."
     train_data = perc.read_labeled_data(opts.trainfile, opts.featfile)
     print >>sys.stderr, "done."
-    feat_vec = perc_train(train_data, tagset, int(opts.n))
+    feat_vec = perc_train(train_data, tagset, int(opts.numepochs))
     perc.perc_write_to_file(feat_vec, opts.modelfile)
-
