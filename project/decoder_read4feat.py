533d9a42be3490fe0d2cddfbf60767d8232789d6
weipuz <zhaoweipu.tju@gmail.com>
2014-12-02 21:57:40 -0800

    nbest output added

--- project/decoder_read4feat.py
+++ project/decoder_read4feat.py
@@ -33,14 +33,15 @@ optparser.add_option("-i", "--input", dest="input", default="test/all.cn-en.cn",
 optparser.add_option("-t", "--translation-model", dest="tm", default="large/phrase-table/test-filtered/rules_cnt.final.out", help="File containing translation model (default=data/tm)")
 optparser.add_option("-l", "--language-model", dest="lm", default="lm/en.gigaword.3g.filtered.dev_test.arpa.gz", help="File containing ARPA-format language model (default=data/lm)")
 optparser.add_option("-n", "--num_sentences", dest="num_sents", default=sys.maxint, type="int", help="Number of sentences to decode (default=no limit)")
-optparser.add_option("-k", "--translations-per-phrase", dest="k", default=5, type="int", help="Limit on number of translations to consider per phrase (default=1)")
-optparser.add_option("-s", "--stack-size", dest="s", default=5, type="int", help="Maximum stack size (default=5)")
+optparser.add_option("-k", "--translations-per-phrase", dest="k", default=20, type="int", help="Limit on number of translations to consider per phrase (default=1)")
+optparser.add_option("-s", "--stack-size", dest="s", default=100, type="int", help="Maximum stack size (default=5)")
 optparser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,  help="Verbose mode (default=off)")
 optparser.add_option("-e", "--eta", dest="eta", default=float(-5), type= "float") 
 optparser.add_option("-d", "--distortionLimit", dest = "d", type ="int",default = 10)
 opts = optparser.parse_args()[0]
 
 tm = models.TM(opts.tm, opts.k)
+# new tm model read phrase table and present four prob:  p(e|f), p(f|e), two lexically- weighted phrase  lex(e|f) and lex(f|e)
 
 lm = models.LM(opts.lm)
 french = [tuple(line.strip().split()) for line in open(opts.input).readlines()[:opts.num_sents]]
@@ -48,8 +49,8 @@ french = [tuple(line.strip().split()) for line in open(opts.input).readlines()[:
 # tm should translate unknown words as-is with probability 1
 for word in set(sum(french,())):
   if (word,) not in tm:
-    tm[(word,)] = [models.phrase(word, 0.0)]
-
+    tm[(word,)] = [models.phrase(word, [0.0,0.0,0.0,0.0])]
+w = [0.2,0.2,0.2,0.2,0.2,0.2]
 sys.stderr.write("Decoding %s...\n" % (opts.input,))
 for idx, f in enumerate(french):
   # The following code implements a monotone decoding
@@ -57,9 +58,10 @@ for idx, f in enumerate(french):
   # Hence all hypotheses in stacks[i] represent translations of 
   # the first i words of the input sentence. You should generalize
   # this so that they can represent translations of *any* i words.
-  hypothesis = namedtuple("hypothesis", "logprob, lm_state, predecessor, phrase, coverageVec, fpos, penalLogprob")
+  hypothesis = namedtuple("hypothesis", "logprob, lm_state, predecessor, phrase, coverageVec, fpos, slogprob")
   initial_bitmap = bitmap([])
-  initial_hypothesis = hypothesis(0.0, lm.begin(), None, None, initial_bitmap, 0, 0.0)
+  slogprob = namedtuple("slogprob", "lms, rs, pfe, lfe, pef, lef")
+  initial_hypothesis = hypothesis(0.0, lm.begin(), None, None, initial_bitmap, 0, slogprob(0.0,0.0,0.0,0.0,0.0,0.0))
   stacks = [{} for _ in f] + [{}]
   stacks[0][lm.begin(),initial_bitmap,0] = initial_hypothesis
   for i, stack in enumerate(stacks[:-1]):
@@ -71,25 +73,36 @@ for idx, f in enumerate(french):
 		  temp_ph = bitmap(range(k,j))
 		  if (temp_ph & h.coverageVec == 0 ) and f[k:j] in tm:
 			for phrase in tm[f[k:j]]:
-			  logprob = h.logprob + phrase.logprob 
+			  logprob_weighted = sum([x*y for x,y in zip(w[2:4], phrase.logprob)])
+			  logprob = h.logprob + logprob_weighted 
 			  lm_state = h.lm_state
+			  #namedtuple cant be replaced, use new varible to store the value and creat new nametuple at the end
+			  newpfe = h.slogprob.pfe + phrase.logprob[1]
+			  newpef = h.slogprob.pef + phrase.logprob[0]
+			  newlfe = h.slogprob.lfe + phrase.logprob[3]
+			  newlef = h.slogprob.lef + phrase.logprob[2]
 			  new_coverage= temp_ph | h.coverageVec
 			  fpos = j
 			  covered = onbits(new_coverage)
+			  newlms = h.slogprob.lms
+			  newrs = h.slogprob.rs
 			  for word in phrase.english.split():
 				(lm_state, word_logprob) = lm.score(lm_state, word)
 				logprob += 0.2*word_logprob
-			  logprob += lm.end(lm_state) if covered == len(f) else 0.0            
-			  penalLogprob = logprob + opts.eta * abs(h.fpos+1-k)
-			  new_hypothesis = hypothesis(logprob, lm_state, h, phrase, new_coverage, fpos, penalLogprob)
+				newlms += word_logprob
+			  logprob += 0.2*lm.end(lm_state) if covered == len(f) else 0.0            
+			  newlms += lm.end(lm_state) if covered == len(f) else 0.0            
+			  newrs += (h.fpos+1-k)
+			  new_slogprob=slogprob(newlms,newrs, newpfe, newlfe, newpef, newlef)
+			  new_hypothesis = hypothesis(logprob, lm_state, h, phrase, new_coverage, fpos, new_slogprob)
 			  if (lm_state, new_coverage, fpos) not in stacks[covered] or stacks[covered][lm_state, new_coverage, fpos].logprob < logprob: # second case is recombination
 				stacks[covered][lm_state, new_coverage, fpos] = new_hypothesis 
-  winners = heapq.nlargest(10,stacks[-1].itervalues(), key=lambda h: h.logprob)
+  winners = heapq.nlargest(100,stacks[-1].itervalues(), key=lambda h: h.logprob)
   def extract_english(h): 
     return "" if h.predecessor is None else "%s%s " % (extract_english(h.predecessor), h.phrase.english)
   
   for winner in winners:
-	print str(idx) + " ||| " + extract_english(winner) + " ||| " + str(winner.logprob)
+	print str(idx) + " ||| " + extract_english(winner) + " ||| "  + " ".join([str(i) for i in winner.slogprob])
   if opts.verbose:
     def extract_tm_logprob(h):
       return 0.0 if h.predecessor is None else h.phrase.logprob + extract_tm_logprob(h.predecessor)
